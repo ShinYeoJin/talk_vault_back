80be66bdf922a7346c63555323bee816f82bd20d
ShinYeoJin <syj3072@naver.com>
2026-01-02 11:06:38 +0900

    supabase

--- src/common/supabase/supabase.client.ts
+++ src/common/supabase/supabase.client.ts
@@ -1,9 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  }
 );
+
 
 
 
